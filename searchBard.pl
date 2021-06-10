ff29834d267fea756559e91851fd57c70ca9cd46
mindfu23 <jim.beach@gmail.com>
2021-06-10 11:49:31 -0700

    Add files via upload

---
+++ searchBard.pl
@@ -0,0 +1,20 @@
+# Python script for searching named searchBard
+
+do sendRequest(companies, jobTitle):
+	#text
+
+
+do parseReturn(webtext):
+	#text
+
+
+do formatResults(returnedText):
+
+	
+do outputPage(stuff):
+	print("got here")
+	print(stuff)
+
+outputPage("This text")
+
+
